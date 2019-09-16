8c003d2c5cfc07add5465696f5c95f844caa33ce
feiltel <wy871139697@163.com>
2019-09-16 11:52:59 +0800

    加入下拉刷新功能

--- app/src/main/java/com/nut2014/kotlintest/view/HomeActivity.kt
+++ app/src/main/java/com/nut2014/kotlintest/view/HomeActivity.kt
@@ -3,16 +3,20 @@ package com.nut2014.kotlintest.view
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.StaggeredGridLayoutManager
 import android.view.View
 import com.bumptech.glide.Glide
 import com.chad.library.adapter.base.BaseQuickAdapter
+import com.liaoinstan.springview.widget.SpringView
+import com.liaoinstan.springview.widget.SpringView.OnFreshListener
 import com.nut2014.kotlintest.R
+import com.nut2014.kotlintest.adapter.AliFooter
+import com.nut2014.kotlintest.adapter.AliHeader
 import com.nut2014.kotlintest.adapter.HomeListAdapter
 import com.nut2014.kotlintest.base.BaseApplication
 import com.nut2014.kotlintest.entity.Cover
 import com.nut2014.kotlintest.network.runRxLambda
 import kotlinx.android.synthetic.main.activity_home.*
-import org.jetbrains.anko.appcompat.v7.expandedMenuView
 import org.jetbrains.anko.toast
 import java.util.*
 
@@ -46,20 +50,35 @@ class HomeActivity : AppCompatActivity() {
      * 设置View
      */
     private fun setView() {
-        toolbar_tb.setOnClickListener(View.OnClickListener {
-            list_rv.smoothScrollToPosition(0)
+        toolbar_tb.setOnClickListener {
+            list_rv.scrollToPosition(0)
             top_AppBarLayout.setExpanded(true)
-        })
+        }
         list_rv.adapter = adapter
-
+        adapter.openLoadAnimation()
         list_rv.layoutManager = GridLayoutManager(this, 2)
+      //  list_rv.layoutManager = StaggeredGridLayoutManager(2, 1)
         adapter.onItemClickListener = BaseQuickAdapter.OnItemClickListener { adapter, view, position ->
-            toast("$position>>>>${dataList.get(position).coverDes}")
+            toast("$position>>>>${dataList[position].coverDes}")
+            Glide.with(this@HomeActivity).load(dataList[position].coverImgPath).into(top_iv)
         }
         adapter.setOnLoadMoreListener({
-
             getUserListData(pageInt)
         }, list_rv)
+        list_sv.header=AliHeader(this@HomeActivity,false)
+     //   list_sv.footer=AliFooter(this@HomeActivity,false)
+        list_sv.setListener(object : OnFreshListener {
+            override fun onRefresh() {
+                pageInt=1
+                getUserListData(pageInt)
+
+            }
+
+            override fun onLoadmore() {
+               // getUserListData(pageInt)
+            }
+        })
+
     }
 
     /**
@@ -67,15 +86,20 @@ class HomeActivity : AppCompatActivity() {
      */
     private fun getUserListData(pageNumber: Int = 1) {
         runRxLambda(BaseApplication.App().getService().getCovers(pageNumber), {
+            if(pageNumber==1){
+                dataList.clear()
+                adapter.notifyDataSetChanged()
+            }
+            list_sv.onFinishFreshAndLoad()
             if (it.code == 1) {
-                if (it.data.isNotEmpty()){
+                if (it.data.isNotEmpty()) {
                     Glide.with(this@HomeActivity).load(it.data[0].coverImgPath).into(top_iv)
                 }
 
                 adapter.addData(it.data)
-                if (it.pageNum==it.pages){
+                if (it.pageNum == it.pages) {
                     adapter.loadMoreEnd()
-                }else{
+                } else {
                     pageInt++
                     adapter.loadMoreComplete()
                 }
@@ -83,8 +107,10 @@ class HomeActivity : AppCompatActivity() {
                 adapter.loadMoreFail()
             }
         }, {
+            list_sv.onFinishFreshAndLoad()
             it?.printStackTrace()
             adapter.loadMoreFail()
+
         })
     }
 }
--- app/src/main/java/com/nut2014/kotlintest/view/TestActivity.java
+++ app/src/main/java/com/nut2014/kotlintest/view/TestActivity.java
@@ -3,6 +3,8 @@ package com.nut2014.kotlintest.view;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import com.liaoinstan.springview.widget.SpringView;
 import com.nut2014.kotlintest.R;
 
 public class TestActivity extends AppCompatActivity {
@@ -13,5 +15,19 @@ public class TestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
        Context context= TestActivity.this;
+        final SpringView listSv = findViewById(R.id.list_sv);
+
+        listSv.setListener(new SpringView.OnFreshListener() {
+            @Override
+            public void onRefresh() {
+                listSv.onFinishFreshAndLoad();
+            }
+
+            @Override
+            public void onLoadmore() {
+
+            }
+        });
     }
+
 }
